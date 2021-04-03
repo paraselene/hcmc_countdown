840e017c0b19415d87eaae29830d5964adcd7767
Alex Fung <alex.fung@gallagher.com>
2021-04-04 01:37:08 +1300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,21 +18,20 @@ export class AppComponent {
 
     const urlParams = new URLSearchParams(window.location.search);
     const countDownTime = urlParams.get('time');
-    const countDownDate = new Date(countDownTime).getTime();
+    const countDownDate = new Date('Jan1,2099,00:' + countDownTime).getTime();
     setInterval(() => {
       const now = new Date().getTime();
       const distance = countDownDate - now;
       const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      let minutes: string = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
-      let seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
-      if (minutes.length < 2) {
-        minutes = '0' + minutes;
-      }
-      if (seconds.length < 2) {
-        seconds = '0' + seconds;
-      }
-      this.time = hours + '時' + minutes + '分' + seconds + '秒';
-      //this.time = countDownTime
+      const minutes: string = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)).toString();
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000).toString();
+      // if (minutes.length < 2) {
+      //   minutes = '0' + minutes;
+      // }
+      // if (seconds.length < 2) {
+      //   seconds = '0' + seconds;
+      // }
+      this.time = minutes + '分' + seconds + '秒';
     }, 1000);
   }
 }
