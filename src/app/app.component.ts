cb17573246417147c4b776b1eedbb2cb2c1d1d70
Alex Fung <alex.fung@gallagher.com>
2021-04-09 00:10:38 +1200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,28 @@ export class AppComponent {
   time: string;
   end = false;
   name: string;
+
+  list = ['復活期第二主日', '復活期第三主日', '復活期第四主日', '復活期第五主日', '復活期第六主日', '復活期第七主日', '聖靈降臨主日', '三一主日', '聖靈降臨後第二主日', '聖靈降臨後第三主日', '聖靈降臨後第四主日', '聖靈降臨後第五主日', '聖靈降臨後第六主日', '聖靈降臨後第七主日', '聖靈降臨後第八主日', '聖靈降臨後第九主日', '聖靈降臨後第十主日', '聖靈降臨後第十一主日', '聖靈降臨後第十二主日', '聖靈降臨後第十三主日', '聖靈降臨後第十四主日', '聖靈降臨後第十五主日', '聖靈降臨後第十六主日', '聖靈降臨後第十七主日', '聖靈降臨後第十八主日', '聖靈降臨後第十九主日', '聖靈降臨後第二十主日', '聖靈降臨後第二十一主日', '聖靈降臨後第二十二主日', '聖靈降臨期第二十三主日', '聖靈降臨期第廿四主日', '聖靈降臨期第廿五主日', '基督君王日', '將臨期第一主日', '將臨期第二主日', '將臨期第三主日', '將臨期第四主日', '聖誕期第一主日'];
+
   public constructor() {
 
+  }
+
+  serviceName(): string {
+    const now = new Date().getTime();
+    //const now = new Date('Apr19,2021,23:59:00').getTime();
+    const reference = 1617537600000;
+    const num = Math.floor((now - reference) / (7 * 24 * 3600 * 1000));
+    console.log(num);
+    return this.list[num];
   }
 
   // tslint:disable-next-line:use-lifecycle-interface
   ngOnInit(): void {
 
     const urlParams = new URLSearchParams(window.location.search);
-    const countDownTime = urlParams.get('time');
-    this.name = urlParams.get('name');
+    const countDownTime = urlParams.get('time') || '00:00';
+    this.name = urlParams.get('name') || this.serviceName();
     const countDownDate = new Date('Jan1,2099,00:' + countDownTime).getTime();
     setInterval(() => {
       const now = new Date().getTime();
