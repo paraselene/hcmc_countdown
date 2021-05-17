7a6ffb14c60f5516b4e5f5574af0e82658b462f5
Alex Fung <alex.fung@gallagher.com>
2021-05-17 21:55:12 +1200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Countdown';
+  title = '倒數';
   time: string;
   end = false;
   name: string;
