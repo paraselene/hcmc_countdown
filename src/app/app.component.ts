d608dda556972ac5c8a84fc50b2b8f416d286621
Alex Fung <alex.fung@gallagher.com>
2023-02-03 00:14:55 +1300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,35 +11,21 @@ export class AppComponent {
   end = false;
   name: string;
 
-  list = [
-    "聖誕期第二主日", "基督領洗日", "顯現期第二主日", "顯現期第三主日", "顯現期第四主日", //1月
-    "顯現期第五主日", "顯現期第六主日", "顯現期第七主日", "顯現期末主日", //2月
-    "大齋期第一主日", "大齋期第二主日", "大齋期第三主日", "大齋期第四主日", //3月 
-    "大齋期第五主日", "棕枝主日", "救主復活日", "復活期第二主日", //4月 
-    "復活期第三主日", "復活期第四主日", "復活期第五主日", "復活期第六主日", "復活期第七主日", //5月 
-    "聖靈降臨主日", "三一主日", "聖靈降臨期第二主日", "聖靈降臨期第三主日", //6月 
-    "聖靈降臨期第四主日", "聖靈降臨期第五主日", "聖靈降臨期第六主日", "聖靈降臨期第七主日", "聖靈降臨期第八主日", //7月 
-    "聖靈降臨期第九主日", "聖靈降臨期第十主日", "聖靈降臨期第十一主日", "聖靈降臨期第十二主日", //8月 
-    "聖靈降臨期第十三主日", "聖靈降臨期第十四主日", "聖靈降臨期第十五主日", "聖靈降臨期第十六主日", //9月 
-    "聖靈降臨後第十七主日", "聖靈降臨後第十八主日", "聖靈降臨後第十九主日", "聖靈降臨後第二十主日", "聖靈降臨後第二十一主日", //10月 
-    "聖靈降臨後第二十二主日", "聖靈降臨期第二十三主日", "基督君王日", "將臨期第一主日", //11月 
-    "將臨期第二主日", "將臨期第三主日", "將臨期第四主日", "救主聖誕日" //12月
-  ];
-
   public constructor() {
   }
 
   serviceName(): string {
-    const now = new Date().getTime();
-    //const now = new Date('Jan2,2022,23:59:00').getTime();
-    const reference = new Date('Dec27,2021,00:00:00').getTime();
-    const num = Math.floor((now - reference) / (7 * 24 * 3600 * 1000));
-    return this.list[num];
+    //const now = new Date();
+    const now = new Date('Jan8,2023,14:00:00');
+    const now_timestamp = now.getTime();
+    const reference_timestamp = new Date('Dec25,2022,00:00:00').getTime();
+    const week = Math.floor((now_timestamp - reference_timestamp) / (7 * 24 * 3600 * 1000));
+    const name = ((now.getDate()) < 7) ? "聖餐" : "";
+    return "第" + week + "週"+ name;
   }
 
   // tslint:disable-next-line:use-lifecycle-interface
   ngOnInit(): void {
-
     const urlParams = new URLSearchParams(window.location.search);
     const countDownTime = urlParams.get('time') || '00:00';
     this.name = urlParams.get('name') || this.serviceName();
